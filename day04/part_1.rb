267889f32801f294f2c74e426c39c74d4fb883f2
Dustin Brown <dbrown9@gmail.com>
2021-12-05 11:29:19 -0800

    Update script to actually find the winner!

--- day04/part_1.rb
+++ day04/part_1.rb
@@ -1,3 +1,5 @@
+require_relative "./bingo_board.rb"
+
 input = File.readlines("input.txt", chomp: true)
 
 numbers = input.first.split(",").map(&:to_i)
@@ -6,13 +8,13 @@ current_board = nil
 
 input[1..-1].each do |line|
   if current_board.nil?
-    current_board = []
+    current_board = BingoBoard.new
     next
   end
 
   if line.empty?
     boards << current_board
-    current_board = nil
+    current_board = BingoBoard.new
     next
   end
 
@@ -22,4 +24,19 @@ end
 boards.each do |board|
   puts board
   puts "---"
+end
+
+numbers.each do |number|
+  boards.each do |board|
+    board.mark(number)
+  end
+
+  winner = boards.find { |board| board.bingo? }
+
+  if winner
+    puts "We have a winner!"
+    puts winner
+    puts "won with #{number}"
+    break
+  end
 end
